be6d92cea776b09dfea504fb9def5b86ace8b9bc
Maelton-SI <mls54@aluno.ifal.edu.br>
2023-11-25 09:21:53 -0300

    @ComponentScan(basePackages=)

--- src/main/java/br/com/maelton/controllers/SaudacoesController.java
+++ src/main/java/br/com/maelton/controllers/SaudacoesController.java
@@ -1,4 +1,4 @@
-package br.com.maelton.primeiroprojeto;
+package br.com.maelton.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/br/com/maelton/primeiroprojeto/PrimeiroProjetoApplication.java
+++ src/main/java/br/com/maelton/primeiroprojeto/PrimeiroProjetoApplication.java
@@ -2,9 +2,12 @@ package br.com.maelton.primeiroprojeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 //Classe de inicializacao gerenciada pelo Spring Boot
 @SpringBootApplication
+//Determina o pacote em que os componentes devem ser pesquisados pelo Spring Boot
+@ComponentScan(basePackages="package br.com.maelton")
 public class PrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
